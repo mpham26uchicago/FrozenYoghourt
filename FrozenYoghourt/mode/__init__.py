841159d2218b81e530457386f37b1662e5cf7d76
pmont056 <petermontgomery056@gmail.com>
2022-02-17 09:40:25 +0700

    Fix issues #3 and #4

--- FrozenYoghourt/mode/__init__.py
+++ FrozenYoghourt/mode/__init__.py
@@ -2,14 +2,23 @@ from FrozenYoghourt import *
     
 
 class Mode:
+    
     representation = 'numpy'
-
+    
     @classmethod
-    def toggle(cls):
-        cls.representation = {'numpy': 'sympy',
-                              'sympy': 'numpy'}[cls.representation]
-        print(f'\033[1m{cls.representation.capitalize()}\033[0m mode activated')
+    def toggle(cls, specific_mode = None):
 
+        if specific_mode is None:
+            cls.representation = {'numpy': 'sympy',
+                                  'sympy': 'numpy'}[cls.representation]
+            print(f'\033[1;34m{cls.representation.upper()} \033[1;32mactivated')
+        else:
+            cls.representation = {'n': 'numpy', 
+                                  'numpy': 'numpy', 
+                                  's': 'sympy', 
+                                  'sympy':'sympy'}[specific_mode]
+            print(f'\033[1;34m{cls.representation.upper()} \033[1;32mactivated')
+            
     @classmethod
     def now(cls):
-        print(f'\033[1m{cls.representation.capitalize()}\033[0m mode is \033[1mon\033[0m')
+        print(f'\033[1;34m{cls.representation.upper()} \033[1;30mis\033[1;30m \033[1;32mon')
